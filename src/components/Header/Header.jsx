b4801b9529ecd7a03b38c985d3a5ef83ab72980c
jemaxmars <jemaxmars@icloud.com>
2025-07-07 11:33:26 -0400

    basic structure of header complete

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,17 @@
+import "./Header.css";
+import logo from "../../assets/logo.svg";
+import avatar from "../../assets/avatar.svg";
+
 function Header() {
   return (
     <header className="header">
-      <img className="header__logo" />
+      <img className="header__logo" src={logo} />
       <p className="header__date-and-location">DATE, LOCATION</p>
+
       <button className="header__add-clothes-btn">+ Add clothes</button>
       <div className="header__user-container">
-        <p className="header__username">NAME</p>
-        <img src="" alt="" className="header__avatar" />
+        <p className="header__username">Terrance Tegegne</p>
+        <img src={avatar} alt="Terrance Tegegne" className="header__avatar" />
       </div>
     </header>
   );
