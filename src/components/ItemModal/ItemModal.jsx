2923c7cb9c3ab2626809c3f043cb7ac3bf3ce383
jemaxmars <jemaxmars@icloud.com>
2025-07-08 17:54:10 -0400

    styling for itemModal.css

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -1,17 +1,25 @@
 import "./ItemModal.css";
-import modalClose from "../../assets/modalClose.svg";
+import itemModalClose from "../../assets/itemModalClose.svg";
 
 function ItemModal({ activeModal, onClose, card }) {
   return (
     <div className={`modal ${activeModal === "preview" && "modal__opened"}`}>
       <div className="modal__content modal__content_type_image">
-        <button onClick={onClose} type="button" className="modal__close">
-          <img src={modalClose} alt="X-Icon" className="modal__close-icon" />
+        <button
+          onClick={onClose}
+          type="button"
+          className="modal__close_type_preview"
+        >
+          <img
+            src={itemModalClose}
+            alt="X-Icon"
+            className="modal__close-icon_type_preview"
+          />
         </button>
         <img src={card.link} alt="" className="modal__image" />
         <div className="modal__footer">
-                  <h2 className="modal__caption">{card.name}</h2>
-                  <p className="modal__weather">Weather: {card.weather}</p>
+          <h2 className="modal__caption">{card.name}</h2>
+          <p className="modal__weather">Weather: {card.weather}</p>
         </div>
       </div>
     </div>
