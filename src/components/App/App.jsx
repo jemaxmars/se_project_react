7f261952f12f570b5c26a9af5053df4625103ca4
jemaxmars <jemaxmars@icloud.com>
2025-07-08 12:40:27 -0400

    styling for ModalWithForm

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
+
 import "./App.css";
 import Header from "../Header/Header";
 import Main from "../Main/Main"
+import ModalWithForm from "../ModalWithForm/ModalWithForm";
 
 function App() {
   const [weatherData, setWeatherData] = useState({ type: "hot" });
@@ -12,6 +14,7 @@ function App() {
         <Header />
         <Main weatherData={weatherData} />
       </div>
+      <ModalWithForm />
     </div>
   );
 }
