07d89a554869fa279985385fd92ce6e407b92456
jemaxmars <jemaxmars@icloud.com>
2025-07-08 14:27:11 -0400

    The ItemModal jsx

---
+++ src/components/ItemModal/ItemModal.jsx
@@ -0,0 +1,21 @@
+import "./ItemModal.css";
+import modalClose from "../../assets/modalClose.svg";
+
+function ItemModal({ activeModal, onClose, card }) {
+  return (
+    <div className={`modal ${activeModal === "preview" && "modal__opened"}`}>
+      <div className="modal__content modal__content_type_image">
+        <button onClick={onClose} type="button" className="modal__close">
+          <img src={modalClose} alt="X-Icon" className="modal__close-icon" />
+        </button>
+        <img src={card.link} alt="" className="modal__image" />
+        <div className="modal__footer">
+                  <h2 className="modal__caption">{card.name}</h2>
+                  <p className="modal__weather">Weather: {card.weather}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ItemModal;
--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,12 +1,12 @@
 import "./ModalWithForm.css";
 import modalClose from "../../assets/modalClose.svg";
 
-function ModalWithForm({ children, buttonText, title, activeModal, handleCloseClick }) {
+function ModalWithForm({ children, buttonText, title, activeModal, onClose }) {
   return (
     <div className={`modal ${activeModal === "add-garment" && "modal__opened"}`}>
       <div className="modal__content">
         <h2 className="modal__title">{title}</h2>
-        <button onClick={handleCloseClick} type="button" className="modal__close">
+        <button onClick={onClose} type="button" className="modal__close">
           <img src={modalClose} alt="X-Icon" className="modal__close-icon" />
         </button>
         <form className="modal__form">
