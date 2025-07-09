7b270f7bfa38d1e90351c9759aa40c542240d3d5
jemaxmars <jemaxmars@icloud.com>
2025-07-09 15:55:33 -0400

    API interactions Part 2

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -1,14 +1,16 @@
-import "./WeatherCard.css"
-import sunny from "../../assets/sunny.png"
+import "./WeatherCard.css";
+import sunny from "../../assets/sunny.png";
 
+function WeatherCard({ weatherData }) {
+  const temperature =
+    weatherData && weatherData.temp ? weatherData.temp.F : "N/A";
 
-function WeatherCard() {
-    return (
-      <section className="weather-card">
-            <p className="weather-card__temp">75 &deg; F</p>
-            <img src={sunny} alt="sunny" className="weather-card__image" />
-      </section>
-    );
+  return (
+    <section className="weather-card">
+      <p className="weather-card__temp"> {temperature} &deg; F</p>
+      <img src={sunny} alt="sunny" className="weather-card__image" />
+    </section>
+  );
 }
 
 export default WeatherCard;
